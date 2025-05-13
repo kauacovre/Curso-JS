90db973d8cad8682b2d4ef43acd578f1a74632a9
Kauã Covre Martins <kauacovre222@gmail.com>
2025-05-12 22:38:54 -0300

    ex

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velo = 81.4;
+console.log(`A velocidade do seu carro ${velo}km/h`)
+
+if (velo > 80) { // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida e foi multado!`)
+}
+console.log(`Dirija sempre com cuidade e utilize o cinto de segurança!`)
