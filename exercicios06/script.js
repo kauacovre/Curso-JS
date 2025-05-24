9ab7b51c0692c654a2e996b8e3a5a368ad35b9ac
Kauã Covre Martins <kauacovre222@gmail.com>
2025-05-24 20:55:47 -0300

    ex

--- exercicios06/script.js
+++ exercicios06/script.js
@@ -1,18 +1,99 @@
-// exercicio word 01 - obj: variavel A passa a valer o valor da variavel B, e vice-versa
-/*let a = 10
-let b = 20
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-console.log(`${a} | ${b}`)
-let c = a
-a = b
-b = c
-console.log(`${a} | ${b} | ${c}`)*/
+function isNumero(n) {
+    if(Number(n.value) >= 1 && Number(n.value) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
 
-// exercicio word 03 - obj: variaveis A | B | C == D | E | F
-let a = (4/2) + (2/4)
-let b = 4/(2+2/4)
-let c = (4+2)*2-4
-let d = 4/2 + 2/4 // a == d | True
-let e = 4/2+2/4 // b == e | False
-let f = 4+2*2-4 // c == f | False
-console.log(`a = ${a} == d = ${d} && b = ${b} != e = ${e} && c = ${c} != f = ${f} `)
+function inlista(n, l) {
+    if (l.indexOf(Number(n.value)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    res.innerHTML = ''
+    if(isNumero(num) && !inlista(num, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+    } else {
+        window.alert('[ERROR] Verifique os dados e tente novamente')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert(`Adicione números a lista`)
+    } else {
+        let tot = valores. length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            } else {
+                (valores[0] < menor)
+                menor = valores[pos]
+            }
+
+        }
+
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao total, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<br><p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<br><p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<br><p>Ao final da soma teremos ${soma}.</p>`
+        res.innerHTML += `<br><p>A média dos valores digitados é ${media}.</p>`
+    }
+}
+
+
+/* function finalizador() {
+    let total = valores.length
+    res.innerHTML = `Ao todo temos ${total} número(s) cadastrado(s)`
+    res.innerHTML += `<br>O maior número informado foi ${maior}`
+    res.innerHTML += `<br>O menor número informado foi ${menor}`
+    res.innerHTML += `<br>Somando todos os valores temos ${soma}`
+    res.innerHTML += `<br>A média dos valores digitados é ${soma/5}`
+}
+
+let maior = 0
+let menor = 100
+let soma = 0
+
+function adicionar() {
+    res.innerHTML = ''
+    if(isNumero(num) && !inlista(num, valores)) {
+        let n = Number(num.value)
+        let item = document.createElement(`option`)
+        item.text = `Valor ${n} adicionado`
+        item.value = `lista${n}`
+        lista.appendChild(item)
+        valores.push(n)
+        if (maior < n){
+            maior = n
+        } else if (menor > n ){
+            menor = n
+        }
+        soma = soma + n
+    } else {
+        window.alert('[ERROR] Verifique os dados e tente novamente')
+    }
+} 
+*/
--- exercicios07/script.js
+++ exercicios07/script.js
@@ -1,6 +1,6 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#res')
+let num = document.getElementById('txtn')
+let res = document.querySelector('div#resultado')
+let lista = document.querySelector('select#lista')
 let valores = []
 
 function isNumero(n) {
@@ -24,76 +24,11 @@ function adicionar() {
     if(isNumero(num) && !inlista(num, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado`
+        item.text = `${num} adicionado`
         lista.appendChild(item)
     } else {
-        window.alert('[ERROR] Verifique os dados e tente novamente')
+        window.alert(`[ERROR] Um ou mais valores estão incorretos`)
     }
     num.value = ''
     num.focus()
 }
-
-function finalizar() {
-    if (valores.length == 0) {
-        window.alert(`Adicione números a lista`)
-    } else {
-        let tot = valores. length
-        let maior = valores[0]
-        let menor = valores[0]
-        let soma = 0
-        let media = 0
-        for (let pos in valores) {
-            soma += valores[pos]
-            if (valores[pos] > maior){
-                maior = valores[pos]
-            } else {
-                (valores[0] < menor)
-                menor = valores[pos]
-            }
-
-        }
-
-        media = soma / tot
-        res.innerHTML = ''
-        res.innerHTML += `<p>Ao total, temos ${tot} números cadastrados</p>`
-        res.innerHTML += `<br><p>O maior valor informado foi ${maior}.</p>`
-        res.innerHTML += `<br><p>O menor valor informado foi ${menor}.</p>`
-        res.innerHTML += `<br><p>Ao final da soma teremos ${soma}.</p>`
-        res.innerHTML += `<br><p>A média dos valores digitados é ${media}.</p>`
-    }
-}
-
-
-/* function finalizador() {
-    let total = valores.length
-    res.innerHTML = `Ao todo temos ${total} número(s) cadastrado(s)`
-    res.innerHTML += `<br>O maior número informado foi ${maior}`
-    res.innerHTML += `<br>O menor número informado foi ${menor}`
-    res.innerHTML += `<br>Somando todos os valores temos ${soma}`
-    res.innerHTML += `<br>A média dos valores digitados é ${soma/5}`
-}
-
-let maior = 0
-let menor = 100
-let soma = 0
-
-function adicionar() {
-    res.innerHTML = ''
-    if(isNumero(num) && !inlista(num, valores)) {
-        let n = Number(num.value)
-        let item = document.createElement(`option`)
-        item.text = `Valor ${n} adicionado`
-        item.value = `lista${n}`
-        lista.appendChild(item)
-        valores.push(n)
-        if (maior < n){
-            maior = n
-        } else if (menor > n ){
-            menor = n
-        }
-        soma = soma + n
-    } else {
-        window.alert('[ERROR] Verifique os dados e tente novamente')
-    }
-} 
-*/
